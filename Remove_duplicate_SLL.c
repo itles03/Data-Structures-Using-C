621161186477588a8d1d50b78bad2fcafa3421aa
itles03 <phanikumar.venna@gmail.com>
2021-01-17 20:47:27 +0530

    Update Remove_duplicate_SLL.c

--- Remove_duplicate_SLL.c
+++ Remove_duplicate_SLL.c
@@ -6,15 +6,15 @@ struct node{
 	int data;
 	struct node *next;
 };
-void create(struct node **head,int ele);
-void display(struct node **ploy);
+void create_list(struct node **head,int ele);
+void display(struct node **head);
 void remove_duplicate(struct node **head);
 
 int main(){
 	struct node *list=NULL;
 	int choice,ele;
-	printf("\n\t Linked List Operations \n\t ------ ---- ----------");
 	do{
+		printf("\n\n\t Linked List Operations \n\t ------ ---- ----------");
 		printf("\n\n 1. Insert \t 2. Display \t 3. Remove Duplicate \t 4. Exit\n");
 		printf("\n Enter your choice:");
 		scanf("%d",&choice);
@@ -22,7 +22,7 @@ int main(){
 			case 1:
 					printf(" Enter New Element:");
 					scanf("%d",&ele);
-					create_poly(&list,ele);
+					create_list(&list,ele);
 					break;
 			case 2:
 					display(&list);			
@@ -39,20 +39,19 @@ int main(){
 }
 
 //Create Linked List insertion at the end
-void create_poly(struct node **head,int ele){
-	int terms,i;
-	struct node *newterm,*temp;
-		newterm= (struct node*) malloc (sizeof(struct node));
-		if(newterm!=NULL){
-			newterm->data=ele;
-			newterm->next=NULL;
-			if(*head==NULL)			//Very First Node in the list
-			   *head = newterm;
+void create_list(struct node **head,int ele){
+	struct node *newnode,*temp;
+		newnode= (struct node*) malloc (sizeof(struct node));
+		if(newnode!=NULL){
+			newnode->data=ele;
+			newnode->next=NULL;
+			if(*head==NULL)			          //Very First Node in the list
+			   *head = newnode;
 			else{
 				temp=*head;
-				while(temp->next!=NULL)	//Traverse the List
+				while(temp->next!=NULL)	         //Traverse the List
 				    temp= temp->next;
-				temp->next = newterm;		//Insert at the end
+				temp->next = newnode;		//Insert at the end
 			}
 			printf(" Newnode is inserted...!");
 		}
@@ -65,7 +64,7 @@ void display(struct node **head){
 		printf("list is Empty....!");
 		return;
 	}
-	printf("\n\n Given Polynomial:");
+	printf("\n\n Single Linked List:");
 	temp= *head;
 	   while(temp!=NULL){
 	   	   printf(" %d\t ",temp->data);
@@ -80,7 +79,7 @@ void remove_duplicate(struct node **head){
 	while (temp != NULL && temp->next != NULL) {
         temp1 = temp;
          while (temp1->next != NULL){
-            if (temp->data == temp1->next->data)	//identify the duplicate
+            if (temp->data == temp1->next->data)	                //identify the duplicate
 			{	
                 duplicate = temp1->next;
                 temp1->next = temp1->next->next;
